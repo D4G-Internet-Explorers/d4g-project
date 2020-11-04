a023d71c237229a2dc099b0b7b8fb00357c953d0
Youcef Benelguemar <youcef.benelguemar@epita.fr>
2020-11-04 12:59:21 +0100

    test table

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+
+        <title>Attestation de déplacement dérogatoire</title>
+    <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+    <table>
+    <tr>
+        <td>Carmen</td>
+        <td>33 ans</td>
+        <td>Espagne</td>
+   </tr>
+   <tr>
+        <td>Michelle</td>
+        <td>26 ans</td>
+        <td>États-Unis</td>
+   </tr>
+    </table>
+
+    </body>
+</html>
