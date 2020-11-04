5033bc45aef65d5b0b8ac6d93c83712735594aee
Youcef Benelguemar <youcef.benelguemar@epita.fr>
2020-11-04 13:16:37 +0100

    table creation

--- index.php
+++ index.php
@@ -4,21 +4,43 @@
         <meta charset="UTF-8">
 
         <title>Attestation de déplacement dérogatoire</title>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="main.css">
     </head>
     <body>
+    <?php echo '<p>'. 3*4 .'</p>'; ?>
     <table>
-    <tr>
-        <td>Carmen</td>
-        <td>33 ans</td>
-        <td>Espagne</td>
-   </tr>
-   <tr>
-        <td>Michelle</td>
-        <td>26 ans</td>
-        <td>États-Unis</td>
-   </tr>
+    <thead> <!-- En-tête du tableau -->
+        <tr>
+            <th>City Name</th>
+            <th>Code IRIS</th>
+            <th>Rank</th>
+            <th>Name IRIS</th>
+            <th>Population</th>
+            <th>Global Score</th>
+            <th>Access to Numeric interface</th>
+            <th>Access to Information</th>
+            <th>Administrative competence</th>
+            <th>Numeric/school competence</th>
+            <th>Global Access</th>
+            <th>Global Competence</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>City Name</td>
+            <td>Code IRIS</td>
+            <td>Rank</td>
+            <td>Name IRIS</td>
+            <td>Population</td>
+            <td>Global Score</td>
+            <td>Access to Numeric interface</td>
+            <td>Access to Information</td>
+            <td>Administrative competence</td>
+            <td>Numeric/school competence</td>
+            <td>Global Access</td>
+            <td>Global Competence</td>
+        </tr>
+    </tbody>
     </table>
-
     </body>
 </html>
